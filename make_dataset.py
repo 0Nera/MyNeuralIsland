32f044484204c53ea15417595f097d85333d6b59
Aren Elchinyan <a2.dev@yandex.ru>
2023-06-20 18:21:54 +0300

    Создание генератора датасетов

---
+++ make_dataset.py
@@ -0,0 +1,30 @@
+# Путь к папке, содержащей изображения
+path = 'in/'
+
+# Изменение размера изображений до 28x28 (На вход лучше подавать изображение 28 на 28)
+new_size = (28, 28)
+
+# Список для хранения массивов "сплющенных" изображений
+image_array_list = []
+
+# Обход всех изображений в папке и открытие их с помощью библиотеки Pillow
+for file_name in os.listdir(path):
+    try:
+        with Image.open(path + file_name) as img:
+            # Преобразование изображения в RGB-формат и изменение размера
+            img = img.convert('RGB').resize(new_size)
+            # "Сплющивание" изображения и преобразование его в массив numpy
+            img_array = np.array(img).flatten()
+            # Добавление "сплющенного" изображения в список
+            image_array_list.append(img_array)
+    except Exception as E:
+        print(file_name, E)
+
+# Конкатенация всех "сплющенных" изображений в один массив numpy
+dataset = np.concatenate(image_array_list)
+
+# Изменение размерности массива для соответствия количеству элементов и размеру каждого элемента
+dataset = dataset.reshape(-1, 3*new_size[0]*new_size[1])
+
+# Сохранение массива в файл numpy
+np.save('dataset.npy', dataset)
